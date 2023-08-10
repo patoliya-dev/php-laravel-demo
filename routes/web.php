62e6ec39540b99ddca4db3cdc868ecc65533e064
Priya T <pthummar@patoliyainfotech.com>
2023-08-10 15:17:48 +0530

    change in web route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\user\RegisterController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Auth;
 
